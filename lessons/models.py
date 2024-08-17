7dcdd8625a610a76affcb2b54d01b6346e24c81a
shipx <shipxena@gmail.com>
2024-08-17 16:20:05 +0300

    first commit

---
+++ lessons/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    image = models.ImageField(upload_to='lessons/courses', verbose_name='изображение', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='lessons/lessons', verbose_name='изображение', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='курс', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
